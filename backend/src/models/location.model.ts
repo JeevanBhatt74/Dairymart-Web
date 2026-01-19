9db71db355b28300cc9f8f7f33e1629afd470f34
JeevanBhatt74 <iamjeevanrkb@gmail.com>
2026-01-19 16:21:15 +0545

    Remove backend folder from repository

--- backend/src/models/location.model.ts
+++
@@ -1,8 +0,0 @@
-import { Schema, model } from "mongoose";
-
-const locationSchema = new Schema({
-  name: { type: String, required: true }, // e.g., "Kathmandu"
-    isActive: { type: Boolean, default: true }
-});
-
-export const Location = model("Location", locationSchema);
