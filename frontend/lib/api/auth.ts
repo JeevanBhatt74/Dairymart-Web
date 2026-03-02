0bc69897bebdb303016ee40aaca2a88fe50ca1f6
JeevanBhatt74 <iamjeevanrkb@gmail.com>
2026-03-02 11:00:38 +0545

    feat: configuration and updates for api services

--- frontend/lib/api/auth.ts
+++ frontend/lib/api/auth.ts
@@ -33,8 +33,47 @@ export const loginUser = async (loginData: any) => {
     } catch (err: Error | any) {
         throw new Error(
             err.response?.data?.message
-                || err.message
-                || "Login failed"
+            || err.message
+            || "Login failed"
         );
     }
 };
+
+export const forgotPassword = async (email: string) => {
+    try {
+        const response = await axios.post(API.AUTH.FORGOT_PASSWORD, { email });
+        return response.data;
+    } catch (err: Error | any) {
+        throw new Error(
+            err.response?.data?.message
+            || err.message
+            || "Failed to send OTP"
+        );
+    }
+}
+
+export const verifyOTP = async (email: string, otp: string) => {
+    try {
+        const response = await axios.post(API.AUTH.VERIFY_OTP, { email, otp });
+        return response.data;
+    } catch (err: Error | any) {
+        throw new Error(
+            err.response?.data?.message
+            || err.message
+            || "Failed to verify OTP"
+        );
+    }
+}
+
+export const resetPassword = async (data: any) => {
+    try {
+        const response = await axios.post(API.AUTH.RESET_PASSWORD, data);
+        return response.data;
+    } catch (err: Error | any) {
+        throw new Error(
+            err.response?.data?.message
+            || err.message
+            || "Failed to reset password"
+        );
+    }
+}
--- frontend/lib/api/endpoints.ts
+++ frontend/lib/api/endpoints.ts
@@ -3,6 +3,9 @@
 export const API = {
     AUTH: {
         REGISTER: "/api/v1/users/register", // backend ko route path
-        LOGIN: "/api/v1/users/login"
+        LOGIN: "/api/v1/users/login",
+        FORGOT_PASSWORD: "/api/v1/users/forgot-password",
+        VERIFY_OTP: "/api/v1/users/verify-otp",
+        RESET_PASSWORD: "/api/v1/users/reset-password"
     }
 };
